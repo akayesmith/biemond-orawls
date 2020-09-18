0172aada810f51f06ca044b9dc91d50f4f885f33
nchalageri <nchalageri@4impact.com.au>
2020-09-18 05:35:25 +0530

    Added index entry for max thread stuck time for noop mode.

--- lib/puppet/type/wls_server/stuck_thread_max_time.rb
+++ lib/puppet/type/wls_server/stuck_thread_max_time.rb
@@ -1,7 +1,7 @@
 newproperty(:stuck_thread_max_time) do
   include EasyType
 
-  desc 'The max threads constraint time'
+  desc 'The max threads constraint time for tuning'
 
   to_translate_to_resource do | raw_resource|
     raw_resource['stuck_thread_max_time']
